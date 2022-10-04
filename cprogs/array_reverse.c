3044a643be18986573e6a43ed46c2e89429ef636
Sayan Mitra <sayan.mitra@gmail.com>
2022-10-03 22:42:28 -0500

    starting c++

--- cprogs/array_reverse.c
+++ cprogs/array_reverse.c
@@ -3,13 +3,21 @@ int reverse(int *array, int size);
 int print(int *array, int size);
 
 int main(){
-  int numbers[10] = {6,7,8,9,1,2,3,4,0,0};
+  int numbers[10] = {9,8,7,6,5,4,3,2,1,0};
   printf("\n Before \n");
   print(numbers,10);
   reverse(numbers,10);
   printf("\n After \n");
   print(numbers,10);
 
+  int array[3] = {1,3,5};
+  int *ptr = array; /* same as: int *ptr = &array[0]; */
+  int i;
+  for (i=0; i<3; i++, ptr++){
+    *(ptr + 1) = *ptr + 1;
+  }
+  printf("\n \n");
+  print(array,3);
   return 0;
 }
 
@@ -24,7 +32,14 @@ int print(int *array, int size)
 
 int reverse(int *array, int size)
 {
-  
+  int i;
+  int temp;
+  for (i=0;i<size/2;i++)
+    {
+      temp = array[i];
+      array[i] = array[size-1-i];
+      array[size-1-i] = temp;
+    }
 
   return 0;
 }
